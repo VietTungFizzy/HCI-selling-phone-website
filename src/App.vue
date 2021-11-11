69f1a90fd68869ff258cdd820625e7b1126f1799
Fizzy <17521231@gm.uit.edu.vn>
2021-11-12 01:19:56 +0700

    add css framework and basic navigation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <Navigation/>
   </div>
   <router-view/>
 </template>
+<script>
+import Navigation from './components/Navigation.vue'
 
+export default {
+  name: "app",
+  components: {
+    Navigation
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/Navigation.vue
@@ -0,0 +1,33 @@
+<template>
+<header>
+  <nav class="relative flex flex-wrap flex-row items-center justify-between">
+    <div class="branding">
+      <router-link to="/" custom>
+        <img src="../assets/icon.png" width="60" height="60">
+      </router-link>
+    </div>
+    <div class="nav-links">
+      <ul>
+        <router-link active-class="text-pink-color background-transparent font-bold uppercase px-3 py-1 text-xs outline-none focus:outline-none mr-1 mb-1" to="#" id="link">Thị trường</router-link>
+        <router-link active-class="text-pink-color background-transparent font-bold uppercase px-3 py-1 text-xs outline-none focus:outline-none mr-1 mb-1" to="#" id="link">Đánh giá</router-link>
+        <router-link active-class="text-pink-color background-transparent font-bold uppercase px-3 py-1 text-xs outline-none focus:outline-none mr-1 mb-1" to="#" id="link">Khuyến mãi</router-link>
+        <router-link active-class="text-pink-color background-transparent font-bold uppercase px-3 py-1 text-xs outline-none focus:outline-none mr-1 mb-1" to="#" id="link">Liên lạc</router-link>
+      </ul>
+    </div>
+  </nav>
+</header>
+</template>
+<script>
+export default {
+  name: "Navigation"
+}
+</script>
+<style lang="scss" scoped>
+.nav-links {
+  ul {
+    a#link{
+      color: #EC4899;
+    }
+  }
+}
+</style>
