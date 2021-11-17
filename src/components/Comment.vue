d969799109fb77c0314dd182dd098dfc0d232645
Fizzy <17521231@gm.uit.edu.vn>
2021-11-17 21:58:43 +0700

    Add comment and finish post page

---
+++ src/components/Comment.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="flex text-justify">
+  <div class="flex-shrink-0 mr-3">
+    <img class="mt-2 rounded-full w-8 h-8 sm:w-10 sm:h-10" src="Generic-avatar.jpg" alt="">
+  </div>
+  <div class="flex-1 px-4 py-2 sm:px-6 sm:py-4">
+    <p><strong class="mr-2">{{data.author}}</strong> {{data.comment}}</p>
+    <div class="text-right">
+      <span class="text-xs text-gray-400">3:34 PM</span>
+    </div>
+  </div>
+</div>
+</template>
+<script>
+export default {
+  name: "Comment",
+  props: {
+    data: Object
+  }
+}
+</script>
+<style scoped>
+
+</style>
--- src/views/Post.vue
+++ src/views/Post.vue
@@ -2,10 +2,79 @@
 <div class="text-left text-4xl">
   <h1><strong>{{post.title}}</strong></h1>
 </div>
+<div class="flex flex-row mt-2">
+  <div>
+    <img src="Generic-avatar.jpg" class="rounded-full h-10 w-10">
+  </div>
+  <div class="flex items-center ml-2 text-gray-400 text-sm">
+    <span>
+      Bởi <strong class="text-red-500">{{post.author}}</strong>
+    </span>
+    <span class="mx-2 dosh"></span>
+    <span class="mr-5">
+      6 giây trước
+    </span>
+    <span class="text-black cursor-pointer">
+      <i class="far fa-comment"></i> hỏi đáp
+    </span>
+  </div>
+</div>
 <div class="text-justify content-area" v-html="post.content"></div>
+<div class="border-top-bottom text-left pl-8 py-3 font-bold">
+  Bình luận
+</div>
+<div class="p-2 border-b">
+  <textarea
+    class="
+    mt-1
+    block
+    w-full
+    border-solid
+    border
+    border-gray-800"
+    rows="5"
+    placeholder="Bình luận"
+    v-model="commentEntered"
+    ></textarea>
+</div>
+<div class="py-2 border-b text-right">
+  <button
+  class="
+    bg-gray-200
+    text-black
+    active:bg-gray-300
+    font-bold
+    uppercase
+    text-sm
+    px-6
+    py-3
+    rounded
+    shadow
+    hover:shadow-lg
+    outline-none
+    focus:outline-none
+    mr-1
+    mb-1
+    ease-linear
+    transition-all
+    duration-150
+  "
+  type="button"
+  @click="sendComment"
+  >
+  Gửi
+</button>
+</div>
+<div v-if="comments.length !== 0">
+  <Comment v-for="comment in comments" :key="comment.id" :data="comment"/>
+</div>
+<div v-else class="text-center mt-5">
+  <em><Strong>Hiện tại không có bình luận nào</Strong></em>
+</div>
 </template>
 <script>
 import axios from "axios";
+import Comment from "../components/Comment.vue"
 // import jQuery from "jquery";
 // const $ = jQuery;
 // window.$ = $;
@@ -15,9 +84,14 @@ export default {
   props: {
     id: String
   },
+  components: {
+    Comment
+  },
   data() {
     return {
-      post: null
+      post: null,
+      comments: [],
+      commentEntered: "",
     }
   },
   created() {
@@ -26,8 +100,19 @@ export default {
     .then(response => {
       _this.post = response.data.result.data
     })
+    axios.get(`http://localhost:8080/api/comment/comment-post-${this.id}.json`)
+    .then(response => {
+      _this.comments = response.data.result.data
+    })
   },
   methods: {
+    sendComment: function() {
+      this.comments.push({
+        author: "Fizzy",
+        comment: this.commentEntered
+      })
+      this.commentEntered = ""
+    }
   }
 }
 </script>
@@ -39,11 +124,46 @@ export default {
 .content-area::v-deep h4,
 .content-area::v-deep h5,
 {
-  margin-top: 10px;
-  margin-bottom: 10px;
+  margin-top: 20px;
+  margin-bottom: 20px;
 }
 
 .content-area::v-deep h2 {
   font-size: 30px;
+}
+
+.content-area::v-deep a {
+  color: red;
+}
+
+.content-area::v-deep .related-link {
+  margin-top: 10px;
+  margin-bottom: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 30px;
+  list-style: square;
+
+  border-top: 1px solid #E8E7E7;
+  border-bottom: 1px solid #E8E7E7;
+}
+
+.content-area::v-deep .related-link li {
+  margin-top: 5px;
+  margin-bottom: 5px;
+}
+
+.dosh {
+  border: 1px solid #E8E7E7;
+  width: 15px;
+}
+
+.border-top-bottom {
+  border-top: 1px solid #E8E7E7;
+  border-bottom: 1px solid #E8E7E7;
+}
+
+textarea {
+  resize: none;
 }
 </style>
